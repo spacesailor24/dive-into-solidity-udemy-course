dffcc6a7d3cad016a12821551baa49cbe05257d1
Wyatt Barnes <me@wyatt.email>
2023-03-14 09:36:53 -1000

    Complete Lottery contract

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -11,36 +11,49 @@ contract Lottery {
 
     // declaring the constructor
     constructor() {
-        // TODO: initialize the owner to the address that deploys the contract
+        // initialize the owner to the address that deploys the contract
+        owner = msg.sender;
     }
 
     // declaring the receive() function that is necessary to receive ETH
     receive() external payable {
-        // TODO: require each player to send exactly 0.1 ETH
-        // TODO: append the new player to the players array
+        // require each player to send exactly 0.1 ETH
+        require(msg.value == 0.1 ether, "please send exactly .1 ETH");
+        // append the new player to the players array
+        players.push(msg.sender);
     }
 
     // returning the contract's balance in wei
     function getBalance() public view returns (uint256) {
-        // TODO: restrict this function so only the owner is allowed to call it
-        // TODO: return the balance of this address
+        // restrict this function so only the owner is allowed to call it
+        require(msg.sender == owner, "ONLY_OWNER");
+        // return the balance of this address
+        return address(this).balance;
     }
 
     // selecting the winner
     function pickWinner() public {
-        // TODO: only the owner can pick a winner 
-        // TODO: owner can only pick a winner if there are at least 3 players in the lottery
+        // only the owner can pick a winner 
+        require(msg.sender == owner, "ONLY_OWNER");
+        // owner can only pick a winner if there are at least 3 players in the lottery
+        require(players.length >= 3, "NOT_ENOUGH_PLAYERS");
 
         uint256 r = random();
         address winner;
 
-        // TODO: compute an unsafe random index of the array and assign it to the winner variable 
+        // compute an unsafe random index of the array and assign it to the winner variable 
+        uint256 index = r % players.length;
 
-        // TODO: append the winner to the gameWinners array
+        // append the winner to the gameWinners array
+        winner = players[index];
+        gameWinners.push(winner);
 
-        // TODO: reset the lottery for the next round
+        // reset the lottery for the next round
+        delete players;
 
-        // TODO: transfer the entire contract's balance to the winner
+        // transfer the entire contract's balance to the winner
+        (bool result, ) = winner.call{value: getBalance()}("");
+        require(result, "failed to send winnings");
     }
 
     // helper function that returns a big random integer
